153a9721d892e308f4deb9da3b699bbf854b7480
Vaibhav <134364830+pwrvbh-9@users.noreply.github.com>
2024-03-29 09:02:16 +0530

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -2,10 +2,10 @@ export const url = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "GeoDB Cities API KEY",
+    "X-RapidAPI-Key": "GeoDB Cities API KEY", //Use your own API key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
 export const Weather_API_URL = "https://api.openweathermap.org/data/2.5";
-export const Weather_API_KEY = "Open Weather API KEY";
+export const Weather_API_KEY = "Open Weather API KEY"; //Use your own API key here
